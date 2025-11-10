4e69cea33acf240118a83bbd477f13908fe5fbf1
anugrahamanoharan16-creator <anugrahamanoharan16@gmail.com>
2025-11-11 00:53:41 +0530

    add app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Flask app deployed with CI/CD Pipeline!", 200
+def home():
+    return "Hello from Simple CI/CD App using GitHub Actions!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
